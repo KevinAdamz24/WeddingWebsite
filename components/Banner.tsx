02d89f8b09f93a51fd84f8e37f09791360f2af3f
KevinAdamz24 <93513897+KevinAdamz24@users.noreply.github.com>
2023-02-24 12:17:23 +1100

    first commit

---
+++ components/Banner.tsx
@@ -0,0 +1,16 @@
+function Banner() {
+  return <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
+    <div>
+        <h1 className="text-7xl">Samuel and Vennesa's Wedding</h1>
+        <h2 className="mt-5 md:mt-0"> Welcome to our Wedding site</h2>
+    </div>
+
+    <p className="mt-5 md:mt-2 text-gray-400 max-w-sm">
+        Here you can find all the information about the day and 
+        see the photographs and videos that were taken during the day.
+    </p>
+    </div>
+  
+}
+
+export default Banner
